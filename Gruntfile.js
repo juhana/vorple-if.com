ea4f2e2586a89a9d3a67c6f3cbc828b3fcb9660b
Juhana Leinonen <git@nitku.net>
2014-05-09 22:00:19 +0300

    rsync with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function( grunt ) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON( 'package.json' ),
+        rsync: {
+            options: {
+                recursive: true
+            },
+            prod: {
+                options: {
+                    src: "_site/",
+                    dest: grunt.option( "dest" ) || "~/www/vorple",
+                    host: grunt.option( "host" )
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks( 'grunt-rsync' );
+
+    grunt.registerTask( 'deploy', [ 'rsync' ]); // use with command "grunt deploy --host=user@example.com --dest=/remote/path"
+};
