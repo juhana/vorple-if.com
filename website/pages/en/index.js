04a2b0e902f5920207526534d25f55657435029d
Juhana Leinonen <git@nitku.net>
2021-02-23 21:58:52 +0200

    Spaces before links

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -176,9 +176,7 @@ const Demo = props => (
                         <a href="https://hlabrande.itch.io/neon-vertex">Try the demo online</a>
                     </li>
                     <li>
-                        Source code for 
-                        <a href="https://github.com/vorple/contributions/tree/master/games/neon-vertex/inform6">Inform 6</a> and
-                        <a href="https://github.com/vorple/contributions/tree/master/games/neon-vertex/inform7">Inform 7</a>
+                        Source code for <a href="https://github.com/vorple/contributions/tree/master/games/neon-vertex/inform6">Inform 6</a> and <a href="https://github.com/vorple/contributions/tree/master/games/neon-vertex/inform7">Inform 7</a>
                     </li>
                 </ul>
             </p>
